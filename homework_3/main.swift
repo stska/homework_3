97b4457f807c64cff0c6d6a6a2e62d1a11f4f825
Stas <sts_ka@mail.ru>
2019-06-26 23:53:19 +0100

    Initial Commit

---
+++ homework_3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  homework_3
+//
+//  Created by user155176 on 26/06/2019.
+//  Copyright © 2019 user155176. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
